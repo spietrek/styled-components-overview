e187191f31ced64efdbf9d6c44220175633f2ab4
Steve Pietrek <spietrek@gmail.com>
2018-10-15 21:14:30 -0400

    Deck styling updates

--- theme.js
+++ theme.js
@@ -1,38 +1,48 @@
 import { dark } from 'mdx-deck/themes';
+import nightOwl from 'prism-react-renderer/themes/nightOwl';
+
+const green = '#42ff71';
 
 export default {
   ...dark,
-  font: 'Roboto, sans-serif',
+  codeSurfer: {
+    ...nightOwl,
+    showNumbers: false,
+  },
+  font: '"IBM Plex Mono", monospace',
+  monospace: '"IBM Plex Mono", monospace',
   colors: {
     text: '#FFF',
     background: '#1D1F21',
     link: '#0ff',
+    pre: '#33F9FF',
+    code: '#33F9FF',
+    quote: '#5fb3b3',
   },
   transitionTimingFunction: 'linear',
   transitionDuration: '.5s',
   blockquote: {
     fontStyle: 'italic',
-    color: '#5fb3b3',
   },
   li: {
     lineHeight: '1.25',
+    marginLeft: '50px',
   },
   h1: {
     marginBottom: '50px',
+    textTransform: 'uppercase',
+    letterSpacing: '0.1em',
+    fontWeight: 600,
   },
-  h2: {
-    textAlign: 'left',
-  },
-  h3: {
-    textAlign: 'left',
-  },
-  h4: {
-    textAlign: 'left',
-  },
-  pre: {
-    color: '#33F9FF',
-  },
-  code: {
-    color: '#33F9FF',
+  css: {
+    textAlign: 'center',
+    fontSize: '16px',
+    '@media screen and (min-width:64em)': {
+      fontSize: '30px',
+    },
+    '& .Slide > div': {
+      minWidth: '80vw',
+      minHeight: '60vh',
+    },
   },
 };
